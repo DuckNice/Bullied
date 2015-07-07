e3bf05dbcc7e5d2607dafb72079030e0af1ad1d6
ssw2 <sswkasper@hotmail.com>
2015-07-08 00:45:16 +0200

    Changed structure and added simple soundmixer and manager. Has short delay when changing tracks though.

---
+++ Project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MusicTrack
+{
+    Intro,
+    Buildup,
+    Climax,
+    Encore
+}
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    public AudioSource IntroSource;
+    public AudioSource BuildupSource;
+    public AudioSource ClimaxSource;
+    public AudioSource EncoreSource;
+
+
+    private void OnEnable()
+    {
+        Invoke("setTrack", 20);
+    }
+
+    void setTrack()
+    {
+        SetMusicTrack(MusicTrack.Buildup);
+    }
+
+
+    public void SetMusicTrack(MusicTrack newTrack)
+    {
+        StartCoroutine(SwitchSound(newTrack));
+    }
+
+    IEnumerator SwitchSound(MusicTrack newTrack)
+    {
+        IntroSource.loop = false;
+        BuildupSource.loop = false;
+        ClimaxSource.loop = false;
+
+        var playingSource = (IntroSource.isPlaying)
+            ? IntroSource : (BuildupSource.isPlaying) 
+            ? BuildupSource : (ClimaxSource.isPlaying) 
+            ? ClimaxSource : EncoreSource;
+
+        while (true)
+        {
+            if (!playingSource.isPlaying)
+            {
+                switch (newTrack)
+                {
+                    case MusicTrack.Intro:
+                        playingSource = IntroSource;
+                        break;
+                    case MusicTrack.Buildup:
+                        playingSource = BuildupSource;
+                        break;
+                    case MusicTrack.Climax:
+                        playingSource = ClimaxSource;
+                        break;
+                    case MusicTrack.Encore:
+                        playingSource = EncoreSource;
+                        break;
+                }
+
+                playingSource.Play();
+                playingSource.loop = true;
+
+                break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
