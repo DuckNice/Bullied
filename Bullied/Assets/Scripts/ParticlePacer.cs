8dcebb490d45effd9b0a9f2caf9bce32895ee46b
Kasper Ingdahl Andkjær <sswkasper@hotmail.com>
2015-08-07 16:58:44 +0200

    ParticlePacer complete

--- Bullied/Assets/Scripts/ParticlePacer.cs
+++ Bullied/Assets/Scripts/ParticlePacer.cs
@@ -3,29 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ParticlePacer : MonoBehaviour {
-    public ParticleSystem emitter;
+    public Material mat;
+    [SerializeField] private Vector2 _particlesFadeStartDur;
+    [SerializeField] private Vector2 _particlesOverlapStartDur;
 
-    [SerializeField] private AnimationCurve _emitanceProgress;
+    [SerializeField] private Vector2 _particleSystemStartStopZ = new Vector2(6.4f, -0.7f);
+    [SerializeField] private AnimationCurve _particleFadeIn;
+    [SerializeField] private AnimationCurve _particleOverlap;
+    [SerializeField] private int startStep = 2;
+    private float timeStart = -1;
 
     public LevelSetup GameManager;
-
-    private float _duration;
-
+    
     List<Particle> particles = new List<Particle>();
 
     protected void Start()
     {
-        _duration = GameManager.TotalSteps * GameManager.TimeBetweenSteps;
+        mat.SetColor("_TintColor", new Color(0.49f, 0.49f, 0.49f, 1));
     }
 
     protected void Update()
     {
-     /*   for(int i = particles.Count - 1; i >= 0 ; i++)
+        if(startStep <= GameManager.CurrentStep)
         {
-            if(particles[i].)
+            if(timeStart < 0)
             {
-                particles.RemoveAt(i);
+                timeStart = Time.time;
             }
-        }*/
+
+            float alpha = Mathf.Clamp01(_particleFadeIn.Evaluate((Time.time - timeStart - _particlesFadeStartDur.x) / _particlesFadeStartDur.y));
+
+            mat.SetColor("_TintColor", new Color(0.49f, 0.49f, 0.49f, alpha));
+
+            if(alpha == 0)
+            {
+                float pos = Mathf.Clamp01(_particleOverlap.Evaluate((Time.time - timeStart - _particlesFadeStartDur.x - _particlesFadeStartDur.y - _particlesOverlapStartDur.x) / _particlesOverlapStartDur.y));
+
+                float worldPos = (_particleSystemStartStopZ.y * pos) + ((1 - pos) * _particleSystemStartStopZ.x);
+                
+                transform.position = new Vector3(transform.position.x, transform.position.y, worldPos);
+            }
+        }
     }
 }
